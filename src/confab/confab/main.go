3ec07a9f907819542bb2e73c99a435c0465b5c39
Matthew Horan <mhoran@pivotal.io>
2016-05-03 14:11:10 -0400

    Confab waits on the agent to exit when run on Windows
    Signed-off-by: Ben Moss <bmoss@pivotal.io>

--- src/confab/confab/main.go
+++ src/confab/confab/main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"runtime"
 	"time"
 
 	"github.com/cloudfoundry-incubator/consul-release/src/confab"
@@ -145,6 +146,13 @@ func main() {
 			stderr.Printf("error during start: %s", err)
 			r.Stop()
 			os.Exit(1)
+		}
+		if runtime.GOOS == "windows" {
+			if err := agentRunner.Wait(); err != nil {
+				stderr.Printf("error during wait: %s", err)
+				r.Stop()
+				os.Exit(1)
+			}
 		}
 	case "stop":
 		if err := r.Stop(); err != nil {
